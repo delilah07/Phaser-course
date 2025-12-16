8aa0c8286de4e5836981e85c49216be86e23d319
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-12-16 10:55:16 +0100

    add sprite

--- Flappy-Bird/src/index.js
+++ Flappy-Bird/src/index.js
@@ -24,12 +24,18 @@ function preload () {
   // this context - scene
   // contains functions and properties of this scene
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
+
+let bird = null
 
 // initialization 
 function create () {
   // x, y, key of image
   // image has 0,0 coordinates in the middle 
   // this.add.image(0, 0, 'sky');
-  this.add.image(config.width / 2, config.height / 2, 'sky');
+
+  // this.add.image(config.width / 2, config.height / 2, 'sky'); // or
+  this.add.image(0, 0, 'sky').setOrigin(0,0);
+  bird = this.add.sprite(config.width / 10, config.height / 2, 'bird');
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Phaser-course
 
 game dev with Javascript/Typescript and Phaser 3
+
+Flappy Bird Game https://delilah07.github.io/Phaser-course/Flappy-Bird/build/
