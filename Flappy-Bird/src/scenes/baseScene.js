68f7e329740c9aa6484d07a73a9ad754ba950306
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-12-20 18:36:24 +0100

    add menu

--- Flappy-Bird/src/scenes/baseScene.js
+++ Flappy-Bird/src/scenes/baseScene.js
@@ -4,10 +4,23 @@ class BaseScene extends Phaser.Scene{
     constructor(key, config){
         super(key)
         this.config = config
+        this.screenCenter = [config.width/1.25, config.height/1.1]
     }
 
     create(){
         this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    }
+
+    createMenu(menu, setupMenuEvents){
+        let lastMenuPosition = 0
+        menu.forEach(menuItem => {
+            const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPosition];
+            menuItem.textObject = this.add
+                .text(...menuPosition, menuItem.text, {fontSize: '32px', fill: '#0073ff', fontStyle: 'bold', strokeThickness: '5' })
+                .setOrigin(0.5, 1);
+            lastMenuPosition += 42
+            setupMenuEvents(menuItem)
+        });
     }
 }
 
--- Flappy-Bird/src/scenes/menuScene.js
+++ Flappy-Bird/src/scenes/menuScene.js
@@ -3,11 +3,34 @@ import BaseScene from "./baseScene";
 class MenuScene extends BaseScene{
     constructor(config){
         super("MenuScene", config);
+        this.menu = [
+            {scene: 'PlayScene', text: 'Play'},
+            {scene: 'ScoreScene', text: 'Score'},
+            {scene: null, text: 'Exit'},
+        ]
     }
 
     create(){
         super.create()
-        this.scene.start('PlayScene');
+        this.createMenu(this.menu, this.setupMenuIvents.bind(this))
+    }
+
+    setupMenuIvents(menuItem){
+        const textObject = menuItem.textObject
+        textObject.setInteractive()
+
+        textObject.on('pointerover', ()=> {
+            textObject.setStyle({fill: '#2400c7'})
+        })
+        textObject.on('pointerout', ()=> {
+            textObject.setStyle({fill: '#0073ff'})
+        })
+        textObject.on('pointerup', ()=> {
+            menuItem.scene && this.scene.start(menuItem.scene)
+            if(menuItem.text === 'Exit'){
+                this.game.destroy(true)
+            }
+        })
     }
 }
 
