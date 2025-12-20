df77e48aba0f0536e7f27cf1ff04e74079e0687b
Iryna Abrosimova <abrosimovairyna@gmail.com>
2025-12-20 10:48:22 +0100

    add base Scene

--- Flappy-Bird/src/scenes/playScene.js
+++ Flappy-Bird/src/scenes/playScene.js
@@ -1,12 +1,10 @@
-import Phaser from "phaser"
+import BaseScene from "./baseScene"
 
 const PIPES_TO_RENDER =  4
 
-class PlayScene extends Phaser.Scene{
+class PlayScene extends BaseScene{
     constructor(config){
-        super('PlayScene')
-
-        this.config = config
+        super('PlayScene', config)
     
         this.bird = null
         
@@ -23,7 +21,7 @@ class PlayScene extends Phaser.Scene{
     }
 
     create(){
-        this.createBG();
+        super.create()
         this.createBird();
         this.createPipes();
         this.createColliders();
@@ -35,9 +33,6 @@ class PlayScene extends Phaser.Scene{
     update() {
         this.checkGameStatus();
         this.recyclePipe();
-    }
-    createBG() {
-        this.add.image(0, 0, 'sky').setOrigin(0, 0);
     }
     createBird() {
         this.bird = this.physics.add.sprite(this.config.startPosition.x, this.config.startPosition.y, 'bird');
