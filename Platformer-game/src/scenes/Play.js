67c2478d8ee954d53cc0aea446c74256c38bc926
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-03-09 13:34:31 +0100

    add collision with platforms

--- Platformer-game/src/scenes/Play.js
+++ Platformer-game/src/scenes/Play.js
@@ -9,7 +9,9 @@ class Play extends Phaser.Scene {
         const map = this.createMap();
         const layers = this.createLayers(map);
 
-        this.createPlayer();
+        const player = this.createPlayer();
+
+        this.physics.add.collider(player, layers.platformsColliders);
     }
 
     createMap(){
@@ -22,15 +24,21 @@ class Play extends Phaser.Scene {
     createLayers(map){
         const tileset1 = map.getTileset('main_lev_build_1');
         const tileset2 = map.getTileset('main_lev_build_2');
+        const platformsColliders = map.createDynamicLayer('platforms_colliders', tileset1);
         const environment = map.createStaticLayer('environment', [tileset1, tileset2]);
         const platforms = map.createDynamicLayer('platforms', tileset1);
-        return {environment, platforms}
+       
+        platformsColliders.setCollisionByProperty({collides: true});
+        
+        return {environment, platforms, platformsColliders}
     }
 
     createPlayer(){
         const player = this.physics.add.sprite(100, 250, 'player');
         player.body.setGravityY(500);
         player.setCollideWorldBounds(true);
+
+        return player;
     }
     
 }
