54a225a6e1678aa3c6c4de87f76cd24143e50d5f
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-06-22 16:44:54 +0200

    add idle, run and jump with animations

--- Platformer-game/src/entities/Player.js
+++ Platformer-game/src/entities/Player.js
@@ -1,40 +1,66 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
+import initAnimation from './PlayerAnims';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y){
-        super(scene, x, y, 'player');
+  constructor(scene, x, y) {
+    super(scene, x, y, 'player');
 
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
 
-        this.init();
-        this.initEvents();
+    this.init();
+    this.initEvents();
+  }
+
+  init() {
+    this.gravity = 500;
+    this.playerSpeed = 140;
+    this.jumpCount = 0;
+    this.maxJumps = 1;
+    this.cursors = this.scene.input.keyboard.createCursorKeys();
+
+    this.body.setGravityY(this.gravity);
+    this.setCollideWorldBounds(true);
+
+    initAnimation(this.scene.anims);
+  }
+
+  initEvents() {
+    this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this);
+  }
+
+  update() {
+    const { left, right, space, up } = this.cursors;
+    const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space);
+    const isUpJustDown = Phaser.Input.Keyboard.JustDown(up);
+    const onFloor = this.body.onFloor();
+
+    if (left.isDown) {
+      this.setVelocityX(-this.playerSpeed);
+      this.setFlipX(true);
+    } else if (right.isDown) {
+      this.setVelocityX(this.playerSpeed);
+      this.setFlipX(false);
+    } else {
+      this.setVelocityX(0);
     }
 
-    init(){
-        this.gravity = 500;
-        this.playerSpeed = 200;
-        this.cursors = this.scene.input.keyboard.createCursorKeys();
-
-        this.body.setGravityY(this.gravity);
-        this.setCollideWorldBounds(true);
+    if (
+      (isSpaceJustDown || isUpJustDown) &&
+      (onFloor || this.jumpCount <= this.maxJumps)
+    ) {
+      this.setVelocityY(-this.playerSpeed * 2);
+      this.jumpCount++;
     }
 
-    initEvents(){
-        this.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this)
-    }
+    if (onFloor) this.jumpCount = 0;
 
-    update(){
-        const {left, right} = this.cursors;
-
-        if(left.isDown){
-            this.setVelocityX(-this.playerSpeed);
-        } else if(right.isDown){
-            this.setVelocityX(this.playerSpeed);
-        } else {
-            this.setVelocityX(0);
-        }
-    }
+    onFloor
+      ? this.body.velocity.x !== 0
+        ? this.play('run', true)
+        : this.play('idle', true)
+      : this.play('jump', true);
+  }
 }
 
 export default Player;
