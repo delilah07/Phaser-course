cbe28e1b5678e91850a6c2c090939c2a984a659c
Iryna Abrosimova <abrosimovairyna@gmail.com>
2026-07-23 12:11:59 +0200

    refactor and add mixin

---
+++ Platformer-game/src/mixins/collidable.js
@@ -0,0 +1,5 @@
+export default {
+  addCollider(otherGameObj, callback) {
+    this.scene.physics.add.collider(this, otherGameObj, callback, null, this);
+  },
+};
--- Platformer-game/src/scenes/Play.js
+++ Platformer-game/src/scenes/Play.js
@@ -2,43 +2,55 @@ import Phaser from 'phaser';
 import Player from '../entities/Player';
 
 class Play extends Phaser.Scene {
-    constructor() {
-        super('PlayScene')
-    }
+  constructor() {
+    super('PlayScene');
+  }
 
-    create(){
-        const map = this.createMap();
-        const layers = this.createLayers(map);
+  create() {
+    const map = this.createMap();
+    const layers = this.createLayers(map);
 
-       const player = this.createPlayer();
+    const player = this.createPlayer();
 
-        this.physics.add.collider(player, layers.platformsColliders); 
-    }
+    this.createPlayerColliders(player, {
+      colliders: {
+        platformsColliders: layers.platformsColliders,
+      },
+    });
+  }
 
-    createMap(){
-        const map = this.make.tilemap({key: 'map'});
-        map.addTilesetImage('main_lev_build_1', 'tiles-1');
-        map.addTilesetImage('main_lev_build_2', 'tiles-2');
-        return map;
-    }
+  createMap() {
+    const map = this.make.tilemap({ key: 'map' });
+    map.addTilesetImage('main_lev_build_1', 'tiles-1');
+    map.addTilesetImage('main_lev_build_2', 'tiles-2');
+    return map;
+  }
 
-    createLayers(map){
-        const tileset1 = map.getTileset('main_lev_build_1');
-        const tileset2 = map.getTileset('main_lev_build_2');
-        const platformsColliders = map.createDynamicLayer('platforms_colliders', tileset1);
-        const environment = map.createStaticLayer('environment', [tileset1, tileset2]);
-        const platforms = map.createDynamicLayer('platforms', tileset1);
-       
-        platformsColliders.setCollisionByProperty({collides: true});
+  createLayers(map) {
+    const tileset1 = map.getTileset('main_lev_build_1');
+    const tileset2 = map.getTileset('main_lev_build_2');
+    const platformsColliders = map.createDynamicLayer(
+      'platforms_colliders',
+      tileset1,
+    );
+    const environment = map.createStaticLayer('environment', [
+      tileset1,
+      tileset2,
+    ]);
+    const platforms = map.createDynamicLayer('platforms', tileset1);
 
-        return {environment, platforms, platformsColliders}
-    }
+    platformsColliders.setCollisionByProperty({ collides: true });
 
-    createPlayer(){
-        return new Player(this, 100, 250);
-    }
-    
-   
+    return { environment, platforms, platformsColliders };
+  }
+
+  createPlayer() {
+    return new Player(this, 100, 250);
+  }
+
+  createPlayerColliders(player, { colliders }) {
+    player.addCollider(colliders.platformsColliders);
+  }
 }
 
 export default Play;
